8940b15cd94341ed7f62320f5618aa375b3334c4
omarm <omar-massfih@hotmail.com>
2023-12-18 12:26:40 +0100

    Implemented Processing Videos Locally

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,31 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/process-video', (req, res) => {
+    const inputVideoPath = req.body.inputVideoPath;
+    const outputVideoPath = req.body.outputVideoPath;
+    
+    if (!inputVideoPath || !outputVideoPath) {
+        return res.status(400).send("Bad Request: Missing file path");
+    }
+
+    ffmpeg(inputVideoPath)
+        .outputOptions("-vf", "scale=-1:360")
+        .on("end", () => {
+            res.status(200).send("Video processing finished successfully");
+        })
+        .on("error", (err) => {
+            console.log(`An error occurred: ${err.message}`);
+            res.status(500).send(`Internal Server Error;  ${err.message}`);
+        })
+        .save(outputVideoPath);
+        
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Video processing service listening at http://localhost:${port}`);
 });
