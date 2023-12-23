06adcf7ca28a6a6381b5c3a8498359b7e58f8d1a
omarm <omar-massfih@hotmail.com>
2023-12-23 19:20:53 +0100

    Added getVideos function to fetch video documents from Firestore

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -9,8 +9,8 @@ initializeApp();
 
 const firestore = new Firestore();
 const storage = new Storage();
-
 const rawVideoBucketName = "omar-yt-clone-raw-videos";
+const videoCollectionId = "videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -45,4 +45,10 @@ export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
   });
 
   return {url, fileName};
+});
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const querySnapshot =
+    await firestore.collection(videoCollectionId).limit(10).get();
+  return querySnapshot.docs.map((doc) => doc.data());
 });
