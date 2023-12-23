caa0b9fe7718a5abc93b9a2da9ee8318a7435ece
omarm <omar-massfih@hotmail.com>
2023-12-23 19:08:36 +0100

    Implemented adding videos to Firestore

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import ffmpeg from 'fluent-ffmpeg';
 import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setupDirectories, uploadProcessedVideo } from './storage';
+import { isVideoNew, setVideo } from './firestore';
 
 setupDirectories();
 
@@ -24,6 +25,17 @@ app.post('/process-video', async (req, res) => {
 
     const inputFileName = data.name;
     const outputFileName = `processed-${inputFileName}`;
+    const videoId = inputFileName.split('.')[0];
+
+    if (!isVideoNew(videoId)) {
+        return res.status(400).send("Bad Request: video already processing or processed")
+    } else {
+        await setVideo(videoId, {
+            id: videoId,
+            uid: videoId.split('-')[0],
+            status: "processing"
+        });
+    }
 
     await downloadRawVideo(inputFileName);
 
@@ -40,6 +52,11 @@ app.post('/process-video', async (req, res) => {
     }
 
     await uploadProcessedVideo(outputFileName);
+
+    await setVideo(videoId, {
+        status: "processed",
+        filename: outputFileName
+    });
 
     await Promise.all([
         deleteRawVideo(inputFileName),
